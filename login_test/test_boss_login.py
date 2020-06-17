c76d615f3a96c7aeb15e4c79163bdedc648f06bc
Xiaodan666 <pei_xiao_dan@126.com>
2020-06-17 23:09:47 +0800

    企业微信实战1作业 test_login.py test_cookie.py

--- login_test/test_boss_login.py
+++ login_test/test_boss_login.py
@@ -1,5 +1,10 @@
+import json
+from time import sleep
+
 from selenium import webdriver
-import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestBoss:
@@ -7,6 +12,33 @@ class TestBoss:
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-
-    def get_boss_cookie(self):
         self.driver.get("https://www.zhipin.com/")
+
+    def test_get_boss_cookie(self):
+
+        self.driver.find_element(By.CSS_SELECTOR, ".btns a:nth-child(5)").click()
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".scan-login-btn")))
+        self.driver.find_element(By.CSS_SELECTOR, ".scan-login-btn").click()
+        sleep(10)
+        cookies = self.driver.get_cookies()
+        with open("../datas/boss.json", "w") as f:
+            json.dump(cookies, f)
+
+    def test_login_cookies(self):
+        with open("../datas/boss.json") as f:
+            cookies = json.load(f)
+        for cookie in cookies:
+            if "expiry" in cookie.keys():
+                cookie.pop("expiry")
+            self.driver.add_cookie(cookie)
+        while True:
+            self.driver.refresh()
+            res = WebDriverWait(self.driver, 20).until(
+                expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".nav-figure>a")))
+            if res is not None:
+                break
+        sleep(3)
+
+    def teardown(self):
+        self.driver.quit()
